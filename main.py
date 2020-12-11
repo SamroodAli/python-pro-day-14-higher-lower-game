400432238a27b0da936c2cd9c0c44f151c759992
Samrood Ali <samroodali@live.com>
2020-12-11 07:14:05 +0000

    Chore:problem breakdown and create to-do list

---
+++ main.py
@@ -0,0 +1,19 @@
+# Todo 1 :import game_data
+
+# Todo 2 : score, previous_a
+
+# Todo 3 : game function
+    # Todo 3.1 : clear screen and logo
+
+# Todo 4 : assign a if not previous_A else from game data
+
+# Todo 1 : assign b
+
+# Todo 1 :function greater(a,b)
+
+# Todo 1 : recieve input
+
+# Todo 1 : check if correct
+    # Todo 1 : if correct, add score, update previous_a and continue game
+
+    # Todo 1 : else, clear screen, print a,b and score
