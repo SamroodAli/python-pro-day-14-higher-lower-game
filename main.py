f504099fef47af378563af1384792978f78df34a
Samrood Ali <samroodali@live.com>
2020-12-11 07:36:33 +0000

    Add:greater and printing a vs b

--- main.py
+++ main.py
@@ -14,24 +14,28 @@ def game():
     clear()
     print(logo)
 # Todo 4 : assign a if not previous_A else from game data
-if not previous_a:
-    a= random.choice(data)
-else:
-    a = previous_a
-print(a)
+
+    if not previous_a:
+        a= random.choice(data)
+    else:
+        a = previous_a
 # Todo 5 : assign b
-b= random.choice(data)
+    b= random.choice(data)
 
 # Todo 6 :function greater(a,b)
-def greater(a,b):
-    if ['follower_count'] > b['follower_count']:
-        return 'a'
-    else:
-        return 'b'
+    def greater(a,b):
+        if ['follower_count'] > b['follower_count']:
+            return 'a'
+        else:
+            return 'b'
+            
+    greater = greater(a,b)
 # Todo 7:print a and b
-
+    print(f"Compare A: {a['name']}, a {a['description']} from {a['country']}")
+    print(vs)
+    print(f"Compare B: {b['name']}, a {b['description']} from {b['country']}")
 # Todo 8 : recieve input
-
+    
 
 # Todo 9 : check if correct
     # Todo 9.1 : if correct, add score, update previous_a and continue game
