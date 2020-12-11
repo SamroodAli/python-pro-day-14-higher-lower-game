d63f6cc3bc2cc6a1e7ec71927bbdd845b18e6a72
Samrood Ali <samroodali@live.com>
2020-12-11 07:15:51 +0000

    Add: more to-dos

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Todo 1 :import game_data
+# Todo 1 :import game_data,random
 
 # Todo 2 : score, previous_a
 
@@ -7,13 +7,15 @@
 
 # Todo 4 : assign a if not previous_A else from game data
 
-# Todo 1 : assign b
+# Todo 5 : assign b
 
-# Todo 1 :function greater(a,b)
+# Todo 6 :function greater(a,b)
 
-# Todo 1 : recieve input
+# Todo 7 : recieve input
 
-# Todo 1 : check if correct
-    # Todo 1 : if correct, add score, update previous_a and continue game
+# Todo 8 : check if correct
+    # Todo 8.1 : if correct, add score, update previous_a and continue game
 
-    # Todo 1 : else, clear screen, print a,b and score
+    # Todo 8.2 : else, clear screen, print a,b and score
+
+# Todo 9 : continue ? if yes, call game function again
